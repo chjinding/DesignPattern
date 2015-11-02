b46e212a7e43010f97165171b72728f9a0c659ec
chjindig <121262387@qq.com>
2015-11-02 09:13:46 +0800

    Initial commit

---
+++ app/src/main/java/com/design/pattern/designpattern/MainActivity.java
@@ -0,0 +1,13 @@
+package com.design.pattern.designpattern;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
