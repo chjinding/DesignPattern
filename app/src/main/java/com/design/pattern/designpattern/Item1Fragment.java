c4403d6a686f190bc24966b2c706b60c7bf5b864
chjindig <121262387@qq.com>
2015-11-02 15:35:43 +0800

    单例模式

---
+++ app/src/main/java/com/design/pattern/designpattern/Item1Fragment.java
@@ -0,0 +1,19 @@
+package com.design.pattern.designpattern;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Item1Fragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_item1, null);
+
+        return view;
+    }
+}
